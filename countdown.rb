655154b5eea1cd2463f39c1b94c814d2f33705f7
Madison <madivallarino@gmail.com>
2021-09-20 22:55:49 -0400

    commit tree 43fcc011a44ef0c12ab1af07d941de497a7a95d7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(number)
+while number >= 1
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep(1)
+    number -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep number
+    sleep number
+end
+
